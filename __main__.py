72e818410b282eef1caba9f3a471c67030953577
krishnalikhit1701 <95159825+krishnalikhit1701@users.noreply.github.com>
2023-12-18 18:05:24 +0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -234,7 +234,7 @@ class GetInfo:
 
 if __name__=='__main__':
 
-    TARGET = 'https://vk.com/usova212'
+    TARGET = 'miraprinse'
 
     login, password = json.load(open('secret.json'))
     getter = GetInfo(login, password, TARGET, type = 'user_friends', normed = False) # or type = 'group_members'/'user_friends'
